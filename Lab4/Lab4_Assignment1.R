8fb0c4bbd90434bb6e6c1bc0e98fd2f60ed31da2
Christian von Koch <christian.von.koch@gmail.com>
2020-05-21 12:58:48 +0200

    FINAL LAB FINISHED

--- Lab4/Lab4_Assignment1.R
+++ Lab4/Lab4_Assignment1.R
@@ -25,7 +25,6 @@ AR_process_function=function(mu, sigma_sq, T, phi) {
     epsilon=rnorm(1,0,sqrt(sigma_sq))
     result[i]=mu+phi*(result[i-1]-mu)+epsilon
   }
-  print(result)
   return(result)
 }
 
@@ -36,9 +35,13 @@ for (phi in phi_vector) {
   counter=counter+1
 }
 iter=seq(1,200,1)
+counter=1
 for (i in 1:length(phi_vector)) {
-  plot(iter, results_matrix[,i], main="Plot of realization of AR-process", sub=paste("Phi =", phi_vector[i]),
-       xlab="Iteration", ylab="Value", type="l", col="grey")
+  if (counter %% 6 == 0) {
+    plot(iter, results_matrix[,i], main="Plot of realization of AR-process", sub=paste("Phi =", phi_vector[i]),
+         xlab="Iteration", ylab="Value", type="l", col="grey")
+  }
+  counter=counter+1
 }
 
 ## With phi-values below zero the process will oscillate faster but with phi-values above zero the process will
@@ -121,5 +124,139 @@ pairs(fit_y)
 ## around a value between 10 and 20. This is a behaviour similar to what is shown in the posterior for the first
 ## sampler, where phi was set to 0.3 initially, since this distribution was much tighter around the value of 10
 ## for mu.
+
+## c) The data campy.dat contain the number of cases of campylobacter infections in the north of the province
+## Quebec (Canada) in four week intervals from January 1990 to the end of October 2000. It has 13 observations per
+## year and 140 observations in total. Assume that the number of infections ct at each time point follows an 
+## independent Poisson distribution when conditioned on a latend AR(1)-process xt, that is
+## ct given xt ~ Poisson(exp(xt)), where xt is an AR(1)-process as in a). Implement and estimate the model in Stan,
+## using suitable priors of your choice. Produce a plot that contains both the data and the posterior mean and
+## 95 % credible intervals for the latent intensity theta_t=exp(xt) over time. 
+## [Hint: Should xt be seen as data or parameters]
+
+campy=read.table("campy.dat", header=TRUE)
+library(rstan)
+
+StanModel_Pois = '
+data {
+  int<lower=0> T;
+  int c[T];
+}
+
+parameters {
+  real mu;
+  real phi;
+  real<lower=0> sigma;
+  vector[T] x;
+}
+
+model {
+  // Prior
+  phi ~ uniform(-1,1);
+  for (n in 2:T)
+    x[n] ~ normal(mu + phi * (x[n-1]-mu), sigma);
+    
+
+  // Model/likelihood
+  for (n in 1:T)
+    c[n] ~ poisson(exp(x[n]));
+    
+}
+
+generated quantities {
+  vector[T] post_mean;
+  post_mean = exp(x);
+}
+'
+
+data=list(T=dim(campy)[1], c=campy$c)
+fit_pois=stan(model_code=StanModel_Pois, data=data)
+print(fit_pois)
+pois_mean_list=fit_pois@.MISC$summary$msd
+post_mean=pois_mean_list[grep("post_mean", rownames(pois_mean_list)),]
+
+plot(campy$c, col="blue", ylab="No. of infected", xlab="Time")
+points(post_mean[,1], col="black", type="l")
+
+quantiles=fit_pois@.MISC$summary$quan
+quantiles_post_mean=quantiles[grep("post_mean", rownames(quantiles)),]
+cred_interval_post_mean=matrix(0,dim(quantiles_post_mean)[1], 2)
+cred_interval_post_mean[,1]=quantiles_post_mean[,1]
+cred_interval_post_mean[,2]=quantiles_post_mean[,ncol(quantiles_post_mean)]
+
+lines(cred_interval_post_mean[,1], col="gray", lty=21)
+lines(cred_interval_post_mean[,2], col="gray", lty=21)
+title(main="Plot of data vs approximated posterior")
+legend("topleft", box.lty= 1, pch=c(1,NaN,NaN), legend=c("Data", "Posterior mean", "95 % cred. interval"),
+       col=c("blue", "black", "gray"), lwd=c(NaN,1,1), lty=c(NaN, 1, 21))
+
+## As seen in the plot above the posterior mean follows the data accurately. Almost all of the datapoints are
+## inside the credible intervals which aren't that wide which indicates that the approximated posterior
+## resembles the reality shown by the data well. 
+
+## d) Now, assume that we have a prior belief that the true underlying intensity theta_t varies more smoothly than
+## the data suggests. Change the prior for sigma_sq so that it becomes informative about that the AR(1)-process 
+## increments epsilon_t should be small. Re-estimate the model using Stan with the new prior and produce the same
+## plot as in c). Has the posterior for theta_t changed?
+
+StanModel_Pois_Prior = '
+data {
+  int<lower=0> T;
+  int c[T];
+}
+
+parameters {
+  real mu;
+  real phi;
+  real<lower=0> sigma;
+  vector[T] x;
+}
+
+model {
+  // Prior
+  phi ~ uniform(-1,1);
+  sigma ~ scaled_inv_chi_square(140, 0.15);
+  for (n in 2:T)
+    x[n] ~ normal(mu + phi * (x[n-1]-mu), sigma);
+    
+
+  // Model/likelihood
+  for (n in 1:T)
+    c[n] ~ poisson(exp(x[n]));
+    
+}
+
+generated quantities {
+  vector[T] post_mean;
+  post_mean = exp(x);
+}
+'
+fit_pois_prior=stan(model_code=StanModel_Pois_Prior, data=data)
+print(fit_pois_prior)
+pois_mean_list_prior=fit_pois_prior@.MISC$summary$msd
+post_mean_prior=pois_mean_list_prior[grep("post_mean", rownames(pois_mean_list)),]
+
+plot(campy$c, col="blue", ylab="No. of infected", xlab="Time")
+points(post_mean_prior[,1], col="black", type="l")
+
+quantiles_prior=fit_pois_prior@.MISC$summary$quan
+quantiles_post_mean_prior=quantiles_prior[grep("post_mean", rownames(quantiles)),]
+cred_interval_post_mean_prior=matrix(0,dim(quantiles_post_mean)[1], 2)
+cred_interval_post_mean_prior[,1]=quantiles_post_mean_prior[,1]
+cred_interval_post_mean_prior[,2]=quantiles_post_mean_prior[,ncol(quantiles_post_mean)]
+
+lines(cred_interval_post_mean_prior[,1], col="gray", lty=21)
+lines(cred_interval_post_mean_prior[,2], col="gray", lty=21)
+title(main="Plot of data vs approximated posterior")
+legend("topleft", box.lty= 1, pch=c(1,NaN,NaN), legend=c("Data", "Posterior mean", "95 % cred. interval"),
+       col=c("blue", "black", "gray"), lwd=c(NaN,1,1), lty=c(NaN, 1, 21))
+
+## Now when we have specified a small prior for sigma it is noteable in the new plot that the posterior mean
+## varies less and moves more smoothly. The consequence of this is that more datapoints lie outside of the 
+## credible interval which suggests that the approximated posterior does not resemble the reality described by
+## the data as accurately as before. However, by doing this one can avoid overfitting when the model is applied
+## to a new dataset. 
+
+
 
 
